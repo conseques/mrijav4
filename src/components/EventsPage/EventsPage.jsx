77501ea9c5faf88063d44ea63cf726631f45ee15
conseq <57801533+conseques@users.noreply.github.com>
2026-03-16 23:45:51 +0100

    Remove EventsBanner from EventsPage

--- src/components/EventsPage/EventsPage.jsx
+++ src/components/EventsPage/EventsPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReachOut from "../AboutUsPage/ReactOut/ReachOut";
-import EventsBanner from "./EventsBanner/EventsBanner";
 import FeaturedEvent from "./FeaturedEvent/FeaturedEvent";
 import AllEvents from "./AllEvents/AllEvents";
 import PastEvents from "./PastEvents/PastEvents";
@@ -9,7 +8,6 @@ import PastEvents from "./PastEvents/PastEvents";
 const EventsPage = () => {
     return (
         <>
-            <EventsBanner />
             {/*<FeaturedEvent/>*/}
             <AllEvents />
             <PastEvents />
